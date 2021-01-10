3a3948e9147092dbd5f34dd3d327a2a93d7f4559
Matheus Xavier <mattheus.x.a@gmail.com>
2021-01-10 18:17:56 -0300

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react'
+import { fectchProducts } from './api'
+import OrderLocation from './OrderLocation'
+import ProductsList from './ProductsList'
 import StepsHeader from './StepsHeader'
 import './styles.css'
+import { OrderLocationdata, Product } from './types'
 
-function Navbar() {
+function Orders() {
+    const [products, setProducts] = useState<Product[]>([])
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>()
+
+    useEffect(() => {
+        fectchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+
+    }, [])
+
     return (
         <div className="orders-container">
             <StepsHeader/>
+            <ProductsList products={products}/>
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
     )
 }
 
-export default Navbar
+export default Orders
