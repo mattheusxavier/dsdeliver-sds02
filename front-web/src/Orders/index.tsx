e186ffbce77875d1e022cde8449bcba490532c4e
Matheus Xavier <mattheus.x.a@gmail.com>
2021-01-10 09:38:41 -0300

    Add routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import StepsHeader from './StepsHeader'
+import './styles.css'
+
+function Navbar() {
+    return (
+        <div className="orders-container">
+            <StepsHeader/>
+        </div>
+    )
+}
+
+export default Navbar
